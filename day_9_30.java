c8a5b453149a5efb0f91c14cad78c6569286c4be
Chris Wu <83978652+ChrisWu989@users.noreply.github.com>
2025-09-30 13:58:37 -0700

    aa

---
+++ day_9_30.java
@@ -0,0 +1,12 @@
+public class day_9_30 {
+    //create swtich statements for 3 functions and 3 interfaces
+   public static void main(String[] args) {
+        int result = 1;
+
+        if (result != -1) {
+            System.out.println("Element found at index: " + result);
+        } else {
+            System.out.println("Element not found in array.");
+        }
+    }
+}
